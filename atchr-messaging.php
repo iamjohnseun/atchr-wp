4776f3330d3ec1c10939bd6ec1e205804cbca528
John Seun <iamjohnsimeon@gmail.com>
2025-05-01 18:47:12 +0100

    Updates to plugin header

--- atchr-messaging.php
+++ atchr-messaging.php
@@ -1,16 +1,38 @@
 <?php
-/*
-Plugin Name: Atchr Messaging
-Author: Chromesque LLC
-Author URI: https://chromesque.com
-Description: Atchr by Chromesque is a secure AI Chatbot, Instant messaging & Analytics platform for organizations, businesses, teams and individuals. Atchr provides a secure communications platform that bridges the divide between you, your customers and your team.
-Version: 1.0
-*/
+/**
+ * Plugin Name: Atchr Messaging
+ * Plugin URI: https://atchr.com
+ * Description: Atchr by Chromesque is a secure AI Chatbot, Instant messaging & Analytics platform for organizations, businesses, teams and individuals. Atchr provides a secure communications platform that bridges the divide between you, your customers and your team.
+ * Version: 1.0.0
+ * Author: Chromesque, LLC
+ * Author URI: https://chromesque.com
+ * Text Domain: atchr-messaging
+ * Domain Path: /languages
+ * License: GPL v2 or later
+ * License URI: https://www.gnu.org/licenses/gpl-2.0.html
+ * Requires at least: 5.0
+ * Requires PHP: 7.2
+ * Tested up to: 6.8
+ *
+ * Atchr Messaging is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 2 of the License, or
+ * any later version.
+ *
+ * Atchr Messaging is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Atchr Messaging. If not, see https://www.gnu.org/licenses/gpl-2.0.html.
+ */
 
 if (!defined('ABSPATH')) {
   exit;
 }
 
+define('ATCHR_MESSAGING_VERSION', '1.0.0');
 define('ATCHR_MESSAGING_PLUGIN_DIR', plugin_dir_path(__FILE__));
 define('ATCHR_MESSAGING_PLUGIN_URL', plugin_dir_url(__FILE__));
 
