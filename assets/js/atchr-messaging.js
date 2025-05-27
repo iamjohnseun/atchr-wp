a56d4c391a9aebec728cb1ca14cb9c7d8bff4b0e
John Seun <iamjohnsimeon@gmail.com>
2025-05-27 12:09:10 +0100

    Check if embed code exists first, use variable name "entityID"

--- assets/js/atchr-messaging.js
+++ assets/js/atchr-messaging.js
@@ -1,11 +1,12 @@
 (function () {
-  var elem = document.createElement("script"),
-    frame = document.getElementsByTagName("script")[0];
-  elem.async = true;
-  elem.src = "https://embed.atchr.com/" + atchrMessaging.embedCode;
-  elem.charset = "UTF-8";
-  elem.setAttribute("crossorigin", "*");
-  elem.setAttribute("scrolling", "no");
-  elem.setAttribute("allowTransparency", "true");
-  frame.parentNode.insertBefore(elem, frame);
+  if (typeof atchrMessaging !== 'undefined' && atchrMessaging.entityID) {
+    var elem = document.createElement("script"),
+      frame = document.getElementsByTagName("script")[0];
+    elem.async = true;
+    elem.src = "https://embed.atchr.com/" + atchrMessaging.entityID;
+    elem.setAttribute("crossorigin", "*");
+    elem.setAttribute("scrolling", "no");
+    elem.setAttribute("allowTransparency", "true");
+    frame.parentNode.insertBefore(elem, frame);
+  }
 })();
